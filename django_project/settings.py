7d345ec611855f163be6b05c47e86344a9a4dcb0
RajaAnosh521 <javariaali070@gmail.com>
2025-01-02 17:21:14 +0500

    `Added whitenoise to INSTALLED_APPS and updated settings.py`

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,17 +21,13 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True" 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") 
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")   
 
 # Application definition
 
@@ -43,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
