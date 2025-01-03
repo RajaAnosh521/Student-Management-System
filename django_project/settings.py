d546eb69a24e351c1fba6124d56b0c8c07ff1c0f
RajaAnosh521 <javariaali070@gmail.com>
2025-01-03 21:21:19 +0500

    `Revert changes to ALLOWED_HOSTS setting and make it configurable via environment variable`

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,11 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '') 
-# if ALLOWED_HOSTS:
-#     ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
-
-ALLOWED_HOSTS=['student-management-system-production-ed5f.up.railway.app', 'localhost', '127.0.0.1'] 
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '') 
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(",") 
 
 # Application definition
 
